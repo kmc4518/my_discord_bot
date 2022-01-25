e14b7c1b01627299207c78c19edd3226936b30d0
Kevin Cotter <kmc4518@gmail.com>
2022-01-24 21:54:50 -0500

    2022 refresh - pylint & new playlist

--- my_discord_bot.py
+++ my_discord_bot.py
@@ -1,12 +1,19 @@
+"""
+KMC4518 Discord Bot 2022
+"""
+
+import time
 import discord
 import spotipy
 from discord.ext import commands
 from spotipy.oauth2 import SpotifyOAuth
 
 # Secrets
-DISCORD_TOKEN = open("discord_token.txt", "r").readline()
-MASTER_PLAYLIST_TOKEN = open("master_playlist_token.txt", "r").readline()
-YEARONE_PLAYLIST_TOKEN = open("yearone_playlist_token.txt", "r").readline()
+DISCORD_TOKEN = open("discord_token.txt", "r", encoding='UTF-8').readline()
+MASTER_PLAYLIST_TOKEN = open(
+    "master_playlist_token.txt", "r", encoding='UTF-8').readline()
+YEARTWO_PLAYLIST_TOKEN = open(
+    "yeartwo_playlist_token.txt", "r", encoding='UTF-8').readline()
 
 # Define scope & command prexix
 scope = "playlist-modify-public"
@@ -16,26 +23,31 @@ client = commands.Bot(command_prefix="+")
 # Remove default help command
 client.remove_command('help')
 
-# Define command to reply with the ms latency
+
 @client.command()
 async def ping(ctx):
+    """Define command to reply with the ms latency"""
     await ctx.send(f'Pong! {round (client.latency * 1000)}ms ')
 
-# Define command to adds a song to Spotify playlist
+
 @client.command()
 async def add(ctx, *args):
+    """Define command to adds a song to Spotify playlist"""
     tracks = [*args]
     print(tracks)
     sp.playlist_add_items(MASTER_PLAYLIST_TOKEN, tracks)
-    sp.playlist_add_items(YEARONE_PLAYLIST_TOKEN, tracks)
-    await ctx.send("Done")
+    sp.playlist_add_items(YEARTWO_PLAYLIST_TOKEN, tracks)
+    await ctx.send("Song added!")
 
-# Print client detials on startup
+
 @client.event
 async def on_ready():
+    """Print client detials on startup"""
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
 
 client.run(DISCORD_TOKEN)
+
+# 2022!!!!!
