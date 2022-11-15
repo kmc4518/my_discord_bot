7d045f1430c72e35354b64dced7970eb13df53ef
Kevin Cotter <kmc4518@gmail.com>
2022-11-15 00:06:43 -0500

    ENV playlist tokens & defining intent defaults

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
 FROM python:latest
 
+ENV SPOTIPY_CLIENT_ID='<your key here>'
+ENV SPOTIPY_CLIENT_SECRET='<your key here>'
+ENV SPOTIPY_REDIRECT_URI='http://127.0.0.1:9090
+ENV DISCORD_TOKEN='<your key here>'
+ENV MASTER_PLAYLIST_TOKEN='<your key here>'
+ENV YEAR_PLAYLIST_TOKEN='<your key here>'
+
 RUN apt-get update -y
 
 COPY requirements.txt requirements.txt
--- my_discord_bot.py
+++ my_discord_bot.py
@@ -5,20 +5,23 @@ KMC4518 Discord Bot 2022
 import time
 import discord
 import spotipy
+import os
 from discord.ext import commands
 from spotipy.oauth2 import SpotifyOAuth
 
 # Secrets
-DISCORD_TOKEN = open("discord_token.txt", "r", encoding='UTF-8').readline()
-MASTER_PLAYLIST_TOKEN = open(
-    "master_playlist_token.txt", "r", encoding='UTF-8').readline()
-YEARTWO_PLAYLIST_TOKEN = open(
-    "yeartwo_playlist_token.txt", "r", encoding='UTF-8').readline()
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+MASTER_PLAYLIST_TOKEN = os.environ['MASTER_PLAYLIST_TOKEN']
+YEARTWO_PLAYLIST_TOKEN = os.environ['YEAR_PLAYLIST_TOKEN']
 
 # Define scope & command prexix
+
+intents = discord.Intents.default()
+intents.message_content = True
+
 scope = "playlist-modify-public"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-client = commands.Bot(command_prefix="+")
+client = commands.Bot(command_prefix="+", intents=intents)
 
 # Remove default help command
 client.remove_command('help')
